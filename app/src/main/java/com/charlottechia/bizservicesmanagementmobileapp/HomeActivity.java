b3a5bacf7299efdaaa8fce30e509e7a83e12cc45
Haziq200 <17010603@myrp.edu.sg>
2019-05-22 12:48:15 +0800

    Intent from main activity to home activity

--- app/src/main/java/com/charlottechia/bizservicesmanagementmobileapp/HomeActivity.java
+++ app/src/main/java/com/charlottechia/bizservicesmanagementmobileapp/HomeActivity.java
@@ -2,8 +2,13 @@ package com.charlottechia.bizservicesmanagementmobileapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Button btn1;
+    Button btn2;
+    Button btn3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/charlottechia/bizservicesmanagementmobileapp/MainActivity.java
+++ app/src/main/java/com/charlottechia/bizservicesmanagementmobileapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.charlottechia.bizservicesmanagementmobileapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -20,7 +22,17 @@ public class MainActivity extends AppCompatActivity {
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.button);
 
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                   openHomeActivity();
+            }
+        });
 
+    }
 
+    public  void openHomeActivity(){
+        Intent intent = new Intent(getBaseContext(),HomeActivity.class);
+        startActivity(intent);
     }
 }
